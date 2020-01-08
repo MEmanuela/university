1b8e7d73b6458156f44f00f31677887f43ad0a02
Ema <46870621+MEmanuela@users.noreply.github.com>
2020-01-08 14:50:21 +0700

    13.0

--- header.h
+++ header.h
@@ -1,21 +1,117 @@
 #pragma once
-
-#include <conio.h>
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+# include <ctime>
+
 using namespace std;
 
-class Multime
+class StudentAC
 {
-private:
-	int *date = nullptr;
-	int dim; //max
-	int n; //nr curent
+	string nume;
+	int nota;
 public:
-	void adauga(int x);
-	void extrage(int x);
-	void afisare();
-	Multime();
-	Multime(int dim);
-	~Multime();
+	StudentAC()
+	{
+		cout << "constr. fara arg. StudentAC" << endl;
+		nume = nullptr;
+		nota = 0;
+	}
+	StudentAC(string numele, int notax)
+	{
+		cout << "constr. cu arg. PersoanaAC" << endl;
+		nume = numele;
+		nota = notax;
+	}
+	void afisare()
+	{
+		cout << "Nume: " << nume << endl;
+		cout << "Nota: " << nota << endl;
+	}
+	void modificareNota(int nouaNota)
+	{
+		this->nota = nouaNota;
+	}
+	int getNota()
+	{
+		return nota;
+	}
+	string getNume()
+	{
+		return nume;
+	}
+	StudentAC::~StudentAC()
+	{
+		cout << "destructor StudentAC" << endl;
+	}
+	bool operator< (StudentAC s2);
+};vector <StudentAC> arrToVect(StudentAC *v, int n){	vector <StudentAC> rez; 	for (int i = 0; i < n; ++i)
+	{
+		rez.push_back(StudentAC(v[i].getNume(), v[i].getNota()));
+	}	return rez;}bool StudentAC::operator< (StudentAC s2)
+{
+	if (this->getNota() < s2.getNota())
+		return true;
+	else
+		return false;
+}void afisareV(vector<StudentAC> v){	vector<StudentAC>::iterator i;	for (i = v.begin(); i != v.end(); ++i)
+		i->afisare();}
+// clasa masina 
+
+class masina
+{
+	char* marca;
+	char* culoare;
+	double pret;
+	double vit;
+public:
+	masina()
+	{
+		pret = 0;
+		vit = 0;
+		marca = nullptr;
+		culoare = nullptr;
+	}
+	masina(char* m, char* c, double p, double v)
+	{
+		marca = new char[strlen(m)];
+		culoare = new char[strlen(c)];
+		strcpy_s(marca, strlen(m) + 1, m);
+		strcpy_s(culoare, strlen(c) + 1, c);
+		pret = p;
+		vit = v;
+	}
+	double getpret()
+	{
+		return pret;
+	}
+	double getvit()
+	{
+		return vit;
+	}
+	bool operator<(masina m2)
+	{
+		if (this->vit > m2.vit)
+			return true;
+		else
+			if (this->vit = m2.vit)
+				if (this->pret > m2.pret)
+					return true;
+		return false;
+	}
+	void afisarem()
+	{
+		cout << "marca:" << marca << endl;
+		cout << "culoarea:" << culoare << endl;
+		cout << "pret:" << pret << endl;
+		cout << "viteza:" << vit << endl;
+	}
 };
+
+void afisaremas(vector<masina> v)
+{
+	vector<masina>::iterator i;
+	for (i = v.begin();i != v.end();++i)
+		i->afisarem();
+}
--- source.cpp
+++ source.cpp
@@ -1,70 +1,72 @@
 #include "header.h"
 
-Multime::Multime()
-{
-	cout << "constructor implicit";
-	dim = 10; //dim = D
-	date = new int[dim];
-	n = 0;
-}
+using namespace std;
 
-Multime::Multime(int dim)
+int countDigits(string str)
 {
-	this->dim = dim;
-	date = new int[this->dim];
-	n = 0;
-}
-
-Multime::~Multime()
-{
-	cout << "destructor";
-	delete[]date;
-	date = nullptr;
-}
-
-void Multime::adauga(int x)
-{
-	if (n == dim)
+	string::iterator si;
+	int digits = 0;
+	for (si = str.begin(); si != str.end(); ++si)
 	{
-		cout << "full";
-	}
-	else
-	{
-		int ok = 1;
-		for (int i = 0; i < n; i++)
+		if (*si >= '0' && *si <= '9')
 		{
-			if (date[i] == x)
-			{
-				ok = 0;
-			}
-		}
-		if (ok)
-		{
-			date[n] = x;
-			++n;
+			digits++;
 		}
 	}
+	return digits;
 }
 
-void Multime::extrage(int x)
+int numar(string s)
 {
-	for (int i = 0; i < n; i++)
+	int nr = 0;
+	for (int i = 0; i < s.length(); ++i)
 	{
-		if (date[i] == x)
-		{
-			for (int j = 0; j < n -1; j++)
-				date[j] = date[j + 1];
-			n--;
-		}
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			nr = nr + 1;
 	}
+	return nr;
 }
 
-void Multime::afisare()
+int main()
 {
-	cout << "\nElementele vectorului: ";
-	for (int i = 0; i < n; i++)
-	{
-		cout << date[i] << " ";
-	}
-}
+	string s("aB12c3D4fdgHefg6");
+	cout << countDigits(s) << endl;
+	cout << numar(s) << endl;
 
+	StudentAC s1("ion", 9);
+	StudentAC s2("mihai", 7);
+	StudentAC s3("alex", 6);
+
+	vector<StudentAC> v1;
+	v1.push_back(s1);
+	v1.push_back(s2);
+	v1.push_back(s3);
+	afisareV(v1);
+	sort(v1.begin(), v1.end());
+	cout << "dupa sortare" << endl;
+	afisareV(v1);
+
+	vector<int> myvector(800000);
+	generate(myvector.begin(), myvector.end(), rand);
+	double start = clock();
+	sort(myvector.begin(), myvector.end());
+	double stop = clock();
+	cout << "timpul pt sortare: " << ((double)(stop - start)) / CLOCKS_PER_SEC << endl;
+
+	cout << "\tMasini:" << endl;
+	masina m1("ford", "alb", 6500, 250);
+	masina m2("mercedes", "gri", 650, 150);
+	masina m3("bmw", "negru", 12500, 280);
+	vector<masina> m;
+	m.push_back(m1);
+	m.push_back(m2);
+	m.push_back(m3);
+	afisaremas(m);
+	cout << "\tDupa sortare:" << endl;
+	sort(m.begin(), m.end());
+	afisaremas(m);
+
+	system("pause");
+
+	return 0;
+}
